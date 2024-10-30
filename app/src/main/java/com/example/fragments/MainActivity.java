09809609dc0316814f978523b02f70b99fc42d89
AlbertoBarcelo <albertobd6@gmail.com>
2024-10-30 15:55:45 +0100

    dia2

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fragments;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -15,23 +16,24 @@ public class MainActivity extends AppCompatActivity {
         Button buttonFragment1 = findViewById(R.id.button_fragment1);
         Button buttonFragment2 = findViewById(R.id.button_fragment2);
 
-        if (savedInstanceState == null) {
+        // Verifica la orientación del dispositivo
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             if (findViewById(R.id.fragment_container_1) != null && findViewById(R.id.fragment_container_2) != null) {
-
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container_1, new First_Fragment())
                         .replace(R.id.fragment_container_2, new Second_Fragment())
                         .commit();
-            } else {
-
+            }
+        } else {
+            if (findViewById(R.id.fragment_container) != null) {
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new First_Fragment())
                         .commit();
+
+                buttonFragment1.setOnClickListener(v -> replaceFragment(new First_Fragment()));
+                buttonFragment2.setOnClickListener(v -> replaceFragment(new Second_Fragment()));
             }
         }
-
-        buttonFragment1.setOnClickListener(v -> replaceFragment(new First_Fragment()));
-        buttonFragment2.setOnClickListener(v -> replaceFragment(new Second_Fragment()));
     }
 
     private void replaceFragment(Fragment fragment) {
